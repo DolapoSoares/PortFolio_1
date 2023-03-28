094135e76b073e5954e9e9f352bc59b3a7bc93e0
Dolapo Soares <dsoares@econnectnp.com>
2023-03-28 17:29:45 +0100

    setting up the necessary dependencies for the project

---
+++ portfolio-soares/pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import type { NextPage } from "next"
+
+
+
+const Home:NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Soares's Portfolio </title>
+      </Head>
+
+      {/* Header */}
+
+      {/* Her0 */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+     
+    </div>
+  )
+}
+ export default Home
