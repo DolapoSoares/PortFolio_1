c4c70c6449a38e04a09dbacdbe9229f9ba2d1bf3
Dolapo Soares <dsoares@econnectnp.com>
2023-04-03 16:37:27 +0100

    styling the links in the hero section

--- portfolio-soares/components/Hero.tsx
+++ portfolio-soares/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundShape'
+import Link from 'next/link'
 
 
 type Props = {}
@@ -19,18 +20,27 @@ function Hero({}: Props) {
         className='relative rounded-full h-32 w-32 mx-auto object-cover'
         src="https://res.cloudinary.com/soares04/image/upload/v1634422175/uy7y0zkuexy6gevonu7q.png" alt=""
         />
-        <div>
+        <div className='z-20'>
         <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
         <h1 className='text-5xl lg:text-6xl font-semibold px-8'>
             <span className='mr-3'>{text}</span>
             <Cursor cursorColor='#e8a921'/>
         </h1>
 
-        <div>
-            <button>About</button>
-            <button>Experience</button>
-            <button>Skills</button>
-            <button>Project</button>
+        <div className='pt-5'>
+            <Link href="#about">
+                <button className ="heroButton">About</button>
+            </Link>
+            <Link href="#experience">
+                <button className ="heroButton">Experience</button>
+            </Link>
+            <Link href="#skills">
+                <button className ="heroButton">Skills</button>
+            </Link>
+            <Link href="#project">
+                <button className ="heroButton">Project</button>
+            </Link>
+            
         </div>
         </div>
         
