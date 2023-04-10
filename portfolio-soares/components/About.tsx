61288f06d8ceb52e91362f6c028ddca9b4bdacd8
Dolapo Soares <dsoares@econnectnp.com>
2023-04-10 23:01:35 +0100

    About to start creating the Experience components

--- portfolio-soares/components/About.tsx
+++ portfolio-soares/components/About.tsx
@@ -1,39 +1,52 @@
-import React from 'react'
-import { motion } from "framer-motion"
+import React from "react";
+import { motion } from "framer-motion";
 
-type Props = {}
+type Props = {};
 
 function About({}: Props) {
   return (
-    <div className='flex flex-col relative text-center h-screen md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl '>
-            About
-        </h3>
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      transition={{
+        duration: 1.5,
+      }}
+      whileInView={{
+        opacity: 1,
+        x: 0,
+      }}
+      className="flex flex-col relative text-center h-screen md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
+        About
+      </h3>
 
-        <motion.img 
+      <motion.img
         initial={{
-            x : -200, 
-            opacity: 0
+          x: -200,
+          opacity: 0,
         }}
         transition={{
-            duration:1.3
+          duration: 1.3,
         }}
         whileInView={{
-          opacity:1,
-          x:0
+          opacity: 1,
+          x: 0,
         }}
         viewport={{
-          once:true
+          once: true,
         }}
         src="https://res.cloudinary.com/soares04/image/upload/v1680626242/ckexjvtna2qzhelj6cdh.jpg"
-        className='-mb-20 md:mb-0 w-56 h-56 flex-shrink-0 object-cover rounded-full md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]'
-        />
+        className="-mb-20 md:mb-0 w-56 h-56 flex-shrink-0 object-cover rounded-full md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]"
+      />
 
-        <div>
-          
-        </div>
-    </div>
-  )
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">A little summary About Me</h4>
+        <p className="text-base">I am Soares Dolapo</p>
+      </div>
+    </motion.div>
+  );
 }
 
-export default About
+export default About;
--- portfolio-soares/pages/index.tsx
+++ portfolio-soares/pages/index.tsx
@@ -1,36 +1,36 @@
-import Head from 'next/head'
-import type { NextPage } from "next"
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import About from '../components/About'
+import Head from "next/head";
+import type { NextPage } from "next";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Experience from "@/components/Experience";
 
-
-
-const Home:NextPage = () => {
+const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Soares's Portfolio </title>
       </Head>
 
       <Header />
-      <section id="Hero" className='snap-start'>
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
-      {/* About */}
-      <section id="About" className='snap-center'>
+      <section id="about" className="snap-center">
         <About />
       </section>
-      {/* Experience */}
 
+      {/* Experience */}
+      <section id="experience" className="snap-center">
+      <Experience  />
+      </section>
       {/* Skills */}
 
       {/* Projects */}
 
       {/* Contact Me */}
-     
     </div>
-  )
-}
- export default Home
+  );
+};
+export default Home;
