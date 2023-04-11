20a294472bf6b70484daeca85b2ab19eba7fb398
Dolapo Soares <dsoares@econnectnp.com>
2023-04-11 01:00:30 +0100

    Created the experience card component

--- portfolio-soares/components/Experience.tsx
+++ portfolio-soares/components/Experience.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { motion } from "framer-motion";
 
 type Props = {}
 
 function Experience({}: Props) {
   return (
-    <div className='h-screen '>
-        <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
-        About
+    <motion.div className='h-screen flex relative overflow-hidden text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
+        EXPERIENCE
       </h3>
-    </div>
+
+      <div>
+
+      </div>
+    </motion. div>
   )
 }
 
