f5ea45cad6ef468b85341835d4ff4c97a4d7e9a1
Dolapo Soares <dsoares@econnectnp.com>
2023-05-31 00:18:31 +0100

    inputting the various skills learned over the years

--- portfolio-soares/components/Skills.tsx
+++ portfolio-soares/components/Skills.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from "framer-motion";
+import Skill from './Skill'
 
 type Props = {}
 
@@ -8,6 +9,10 @@ function Skills({}: Props) {
     <motion.div className='flex ralative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
         <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-2xl'>Hover over screen for proficency level</h3>
+
+        <div>
+          <Skill />
+        </div>
        
     </motion.div>
   )
