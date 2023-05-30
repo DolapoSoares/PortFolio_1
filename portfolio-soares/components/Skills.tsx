d3a6eff630fcdbb9c98baf2cfce3d6500456e488
Dolapo Soares <dsoares@econnectnp.com>
2023-05-31 00:34:46 +0100

    added relativeness to the skills styling

--- portfolio-soares/components/Skills.tsx
+++ portfolio-soares/components/Skills.tsx
@@ -6,11 +6,21 @@ type Props = {}
 
 function Skills({}: Props) {
   return (
-    <motion.div className='flex ralative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
+    <motion.div 
+    initial={{
+      opacity: 0,
+    }}
+    transition={{
+      duration: 1.5,
+    }}
+    whileInView={{
+      opacity: 1,
+    }}
+    className='flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
         <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-2xl'>Hover over screen for proficency level</h3>
 
-        <div>
+        <div className='grid grid-cols-4 gap-5'>
           <Skill />
           <Skill />
           <Skill />
