e9b51de4a219cbaf8049028a4cdd01387b2cecc5
Dolapo Soares <dsoares@econnectnp.com>
2023-06-06 18:16:51 +0100

    adding the different projects to my portfolio

--- portfolio-soares/components/Projects.tsx
+++ portfolio-soares/components/Projects.tsx
@@ -9,7 +9,13 @@ function Projects({}: Props) {
          Projects
         </h3>
 
-        <div className='w-full absolute top-[30%]'>
+        <div className='relative flex w-full overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+            <div>
+
+            </div>
+        </div>
+
+        <div className='w-full absolute top-[30%] bg-[#f7ab0a]/10 left-0 h-[500px] -skew-y-12'>
 
         </div>
     </div>
