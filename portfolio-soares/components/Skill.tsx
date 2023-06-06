0dd44e44f95804c25890b4ec6612b9e3870bade6
Dolapo Soares <dsoares@econnectnp.com>
2023-06-06 14:53:56 +0100

    giving the skills css grid to get arranged properly

--- portfolio-soares/components/Skill.tsx
+++ portfolio-soares/components/Skill.tsx
@@ -21,12 +21,12 @@ function Skill({ directionLeft }: Props) {
             x: 0
           }}
           src='https://res.cloudinary.com/soares04/image/upload/v1681343173/tl7ntxnjbk8gf0iu4gc9.png'
-          className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-100 ease-in-out "
+          className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-100 ease-in-out"
         />
         
-        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-25 md:h-25 xl:w-32 xl:h-32 rounded-full z-0'>
             <div className='flex items-center justify-center h-full'>
-                <p className='text-3xl font-bold text-black opacity-100'>100%</p>
+                <p className='text-xl font-bold text-black opacity-100'>100%</p>
             </div>
         </div>
     </div>
