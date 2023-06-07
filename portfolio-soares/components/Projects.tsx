e26b54c32794c9e2971e3a050a4fdd6569b98741
Dolapo Soares <dsoares@econnectnp.com>
2023-06-07 17:06:07 +0100

    adding the project images and links

--- portfolio-soares/components/Projects.tsx
+++ portfolio-soares/components/Projects.tsx
@@ -6,16 +6,18 @@ function Projects({}: Props) {
   const projects = [1, 2, 3];
   return (
     <div className="h-screen flex relative flex-col text-left md:flex-row max-w-full xl:px-10 min-h-screen justify-evenly z-0 mx-auto items-center overflow-hidden">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text2xl mt-0">
         Projects
       </h3>
 
       <div className="relative flex w-full overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
         {projects.map(() => {
             return (
-                <div>
-                    <img />
-                    <div></div>
+                <div className="w-screen flex flex-shrink-0 flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen xl:p-64">
+                    <img src="https://res.cloudinary.com/soares04/image/upload/v1686153216/rqcwrp9pcl4inlocgcn5.png" alt=""/>
+                    <div>
+                        <h4>First of 3: PennyCanny</h4>
+                    </div> 
                 </div>
             )
         })}
