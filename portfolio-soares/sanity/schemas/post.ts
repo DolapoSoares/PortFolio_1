66ef14eb37a41823dfb8f5634e0a2e910619496c
Dolapo Soares <dsoares@econnectnp.com>
2023-07-10 13:38:25 +0100

    setting up my portfolio with sanity

--- portfolio-soares/sanity/schemas/post.ts
+++ portfolio-soares/sanity/schemas/post.ts
@@ -5,68 +5,51 @@ export default defineType({
   title: 'PageInfo',
   type: 'document',
   fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
-    }),
-    defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    }),
-    defineField({
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-      fields: [
-        {
-          name: 'alt',
-          type: 'string',
-          title: 'Alternative Text',
-        }
-      ]
-    }),
-    defineField({
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    }),
-    defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    }),
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
-    }),
-  ],
-
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
+    {
+      name:"name",
+      title: "title",
+      type: "string"
     },
-    prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
+    {
+      name:"role",
+      title: "Role",
+      type: "string"
     },
-  },
+    {
+      name:"heroImage",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name:"backgroungInformation",
+      title: "BackgroundInformation",
+      type: "string"
+    },
+    {
+      name:"profilePic",
+      title: "ProfilePic",
+      type: "image",
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name:"phoneNumber",
+      title: "PhoneNumber",
+      type: "string"
+    },
+    {
+      name:"email",
+      title: "Email",
+      type: "string"
+    },
+    {
+      name:"address",
+      title: "Address",
+      type: "string"
+    },
+  ]
 })
