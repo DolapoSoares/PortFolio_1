cefa68c84672399ce5adcfb732f0debc83a6644b
KimberlyPeters <kimberlypeters468@gmail.com>
2024-05-17 17:25:12 +0100

    populating experience section

--- portfolio-soares/components/ExperienceCard.tsx
+++ portfolio-soares/components/ExperienceCard.tsx
@@ -5,9 +5,19 @@ type Props = {
   jobTitle: string;
   company: string;
   role: string;
+  startDate: string;
+  endDate: string;
+  bulletPoints: string[];
 }
 
-function ExperienceCard({}: Props) {
+function ExperienceCard({
+  jobTitle,
+  company,
+  role,
+  startDate,
+  endDate,
+  bulletPoints,
+}: Props) {
   
   return (
     <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] px-10 cursor-pointer transition-opacity duration-200 overflow-hidden'>
@@ -35,10 +45,6 @@ function ExperienceCard({}: Props) {
             <h4 className='text-4xl font-light'>ENGINEER at eConnect</h4>
             <p className='font-bold text-2xl mt-1'>BACKEND ENGINEER</p>
             <div className='flex space-x-2 my-2'>
-              <img className="h-10 w-10 rounded-full" src="https://res.cloudinary.com/soares04/image/upload/v1681343173/tl7ntxnjbk8gf0iu4gc9.png" />
-
-              <img className="h-10 w-10 rounded-full" src="https://res.cloudinary.com/soares04/image/upload/v1681343173/tl7ntxnjbk8gf0iu4gc9.png" />
-              <img className="h-10 w-10 rounded-full" src="https://res.cloudinary.com/soares04/image/upload/v1681343173/tl7ntxnjbk8gf0iu4gc9.png" />
               <img className="h-10 w-10 rounded-full" src="https://res.cloudinary.com/soares04/image/upload/v1681343173/tl7ntxnjbk8gf0iu4gc9.png" />
             </div>
             <p className='uppercase py-5 text-gray-500 '>June 2022 - December 2022</p>
